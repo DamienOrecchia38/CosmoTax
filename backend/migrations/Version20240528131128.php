6c8febb3b65c5f86a6f252ee922645a0776e3f98
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-05-28 15:42:58 +0200

    database creation

---
+++ backend/migrations/Version20240528131128.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240528131128 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users ADD firstname VARCHAR(255) NOT NULL, ADD lastname VARCHAR(255) NOT NULL, ADD adress VARCHAR(255) NOT NULL, ADD phone INT NOT NULL, ADD card_number INT NOT NULL, ADD cryptogram INT NOT NULL, ADD expiration_date DATE NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users DROP firstname, DROP lastname, DROP adress, DROP phone, DROP card_number, DROP cryptogram, DROP expiration_date');
+    }
+}
