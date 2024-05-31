b6c6a1662e35e3311023a4e7c719d697aac36e69
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-05-31 15:56:06 +0200

    Update - SignUpController with validation constraints and error handling

--- backend/src/Controller/SignUpController.php
+++ backend/src/Controller/SignUpController.php
@@ -6,72 +6,109 @@ use App\Entity\User;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\Validator\Constraints\CardScheme;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SignUpController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
     private UserPasswordHasherInterface $passwordHasher;
-    public function __construct(
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher,
-    ) {
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher) {
         $this->entityManager = $entityManager;
         $this->passwordHasher = $passwordHasher;
     }
-    public function __invoke(Request $request): Response
+
+    #[Route('/signup', name: 'app_signup', methods: ['POST'])]
+    public function signup(Request $request): Response
     {
-        // Décode la requête.
         $requestContent = json_decode($request->getContent(), true);
-        // Si les clés "email" et "password" ne sont pas présentent dans la 
-        // requête renvoie une erreur.
-        if (
-            !array_key_exists('email', $requestContent) ||
-            !array_key_exists(
-                'password',
-                $requestContent
-            )
-        ) {
 
-            $message = 'Un problème technique est survenu, veuillez réessayer ultérieu
- return new Response($message, 500)';
+        $validator = Validation::createValidator();
+        
+        $constraints = [
+            'email' => [
+                new NotBlank([
+                    'message' => 'Veuillez entrer une adresse email',
+                ]),
+                new Email([
+                    'message' => 'Veuillez entrer une adresse email valide',
+                ]),
+            ],
+            'password' => [
+                new NotBlank([
+                    'message' => 'Veuillez entrer un mot de passe',
+                ]),
+                new Length([
+                    'min' => 6,
+                    'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                ]),
+            ],
+            'firstname' => new NotBlank(['message' => 'Veuillez entrer votre prénom']),
+            'lastname' => new NotBlank(['message' => 'Veuillez entrer votre nom']),
+            'address' => new NotBlank(['message' => 'Veuillez entrer votre adresse']),
+            'phone' => new NotBlank(['message' => 'Veuillez entrer votre numéro de téléphone']),
+            'cardNumber' => [
+                new NotBlank(['message' => 'Veuillez entrer votre numéro de carte']),
+                new CardScheme(['schemes' => ['VISA', 'MASTERCARD']]),
+            ],
+            'cryptogram' => [
+                new NotBlank(['message' => 'Veuillez entrer votre cryptogramme']),
+                new Length([
+                    'min' => 3,
+                    'max' => 3,
+                    'exactMessage' => 'Votre cryptogramme doit contenir {{ limit }} chiffres',
+                ]),
+            ],
+            'expirationDate' => [
+                new NotBlank(['message' => 'Veuillez entrer la date d\'expiration']),
+                new Date(['message' => 'Veuillez entrer une date valide']),
+                new GreaterThan(['value' => 'today', 'message' => 'La date d\'expiration doit être dans le futur']),
+            ],
+        ];
+
+        $violations = $validator->validate($requestContent, new Collection($constraints));
+
+        if (count($violations) > 0) {
+            $errors = [];
+            foreach ($violations as $violation) {
+                $errors[$violation->getPropertyPath()][] = $violation->getMessage();
+            }
+            return new JsonResponse($errors, Response::HTTP_BAD_REQUEST);
         }
-        $userRoles = $requestContent['roles'];
-        $userEmail = $requestContent['email'];
-        $userPassword = $requestContent['password'];
-        $userFirstName = $requestContent['firstname'];
-        $userLastName = $requestContent['lastname'];
-        $userAdress = $requestContent['adress'];
-        $userPhone = $requestContent['phone'];
-        $userCardNumber = $requestContent['card_number'];
-        $userCryptogram = $requestContent['cryptogram'];
-        $userExpirationDate = $requestContent['expiration_date'];
+
         $userRepository = $this->entityManager->getRepository(User::class);
-        $registeredUser = $userRepository->findOneBy(['email' => $userEmail]);
-        // Si l'utilisateur est déjà enregistré renvoie une erreur.
-        if ($registeredUser) {
-            return new Response('Adresse email déjà enregistrée', 409);
-        }
-        // Hash le mot de passe de l'utilisateur et l'enregistre.
-        $newUser = new User();
-        $userExpirationDate = new DateTime($userExpirationDate);
-        $newUser->setEmail($userEmail);
-        $newUser->setPassword(
-            $this->passwordHasher->hashPassword($newUser, $userPassword)
-        );
-        $newUser->setRoles($userRoles);
-        $newUser->setFirstName($userFirstName);
-        $newUser->setLastName($userLastName);
-        $newUser->setAdress($userAdress);
-        $newUser->setPhone($userPhone);
-        $newUser->setCardNumber($userCardNumber);
-        $newUser->setCryptogram($userCryptogram);
-        $newUser->setExpirationDate($userExpirationDate);
+        $existingUser = $userRepository->findOneBy(['email' => $requestContent['email']]);
 
-        $this->entityManager->persist($newUser);
+        if ($existingUser) {
+            return new JsonResponse(['error' => 'Cette adresse email est déjà utilisée'], Response::HTTP_CONFLICT);
+        }
+
+        $user = new User();
+        $user->setEmail($requestContent['email']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, $requestContent['password']));
+        $user->setFirstName($requestContent['firstname']);
+        $user->setLastName($requestContent['lastname']);
+        $user->setAdress($requestContent['address']);
+        $user->setPhone($requestContent['phone']);
+        $user->setCardNumber($requestContent['cardNumber']);
+        $user->setCryptogram($requestContent['cryptogram']);
+        $user->setExpirationDate(new DateTime($requestContent['expirationDate']));
+
+        $this->entityManager->persist($user);
         $this->entityManager->flush();
-        return new Response('OK', 200);
+
+        return new JsonResponse(['message' => 'Utilisateur créé avec succès'], Response::HTTP_CREATED);
     }
 }
