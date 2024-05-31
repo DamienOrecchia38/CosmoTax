9a4d057238732d00a4ed1e36e111f9ef85a34add
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-05-31 13:53:10 +0200

    Update - home component to include login and signup forms

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,37 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignUpForm';
 
 export default function Home() {
+  
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showSignUpForm, setShowSignUpForm] = useState(false);
+
   return (
-
-    <h1>CosmoTax</h1>
-
+    <div className="min-h-screen">
+      <Navbar 
+        onLoginClick={() => {
+          setShowLoginForm(true);
+          setShowSignUpForm(false);
+        }} 
+        onSignUpClick={() => {
+          setShowSignUpForm(true);
+          setShowLoginForm(false);
+        }} 
+      />
+      {showLoginForm && (
+        <div className="flex justify-center items-center mt-12">
+          <LoginForm />
+        </div>
+      )}
+      {showSignUpForm && (
+        <div className="flex justify-center items-center mt-8">
+          <SignUpForm />
+        </div>
+      )}
+    </div>
   );
 }
