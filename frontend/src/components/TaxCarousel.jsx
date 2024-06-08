da80c89955b4477f107161c7a5f7e88df6ddd8a8
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-06-08 17:52:56 +0200

    Add - loading spinner to TaxCarousel component

--- frontend/src/components/TaxCarousel.jsx
+++ frontend/src/components/TaxCarousel.jsx
@@ -1,13 +1,20 @@
 import Slider from "react-slick";
 import { useEffect, useState } from 'react';
+import { RotatingLines } from 'react-loader-spinner';
 
 export default function TaxCarousel() {
   const [taxes, setTaxes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/api/taxes')
     .then(response => response.json())
-    .then(data => setTaxes(data['hydra:member'] || []))
+    .then(data => {
+        setTaxes(data['hydra:member'] || []);
+        setTimeout(() => {
+            setLoading(false);
+          }, 2000);
+        })
     .catch(error => console.error('Erreur:', error));
   }, []);
 
@@ -39,19 +46,34 @@ export default function TaxCarousel() {
 
   return (
     <div className="text-center">
-        <h1 className="bg-gradient-to-r from-yellow-400 to-green-600 text-transparent bg-clip-text text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold font-['Bangers'] tracking-wider mt-20 mb-20">Payez toutes les taxes de la galaxie au même endroit !</h1>
-        <div className="bg-white bg-opacity-30 shadow-lg backdrop-blur-sm">
+      {loading ? (
+        <div className="flex flex-col justify-center items-center h-screen">
+        <p className="bg-gradient-to-r from-yellow-400 to-green-600 text-transparent bg-clip-text text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold italic font-['Bangers'] tracking-wider mt-20 mb-10">Chargement galactique en cours...</p>
+        <RotatingLines
+          strokeColor="#006400"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="96"
+          visible={true}
+        />
+      </div>
+      ) : (
+        <>
+          <h1 className="bg-gradient-to-r from-yellow-400 to-green-600 text-transparent bg-clip-text text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold font-['Bangers'] tracking-wider mt-20 mb-20">Payez toutes les taxes de la galaxie au même endroit !</h1>
+          <div className="bg-white bg-opacity-30 shadow-lg backdrop-blur-sm">
             <Slider {...settings}>
-            {taxes.map(tax => (
+              {taxes.map(tax => (
                 <div key={tax.id} className="text-center mt-20 mb-10 px-4 sm:px-6 lg:px-8">
-                    <img src={`/images/taxes/${tax.title.replace(/\s+/g, '_').toLowerCase()}.jpg`} alt={tax.title} className="w-full h-48 sm:h-64 md:h-80 lg:h-[30rem] object-cover mb-4 rounded-xl" />
-                    <p className="text-2xl font-bold mb-4">{tax.title}</p>
-                    <p className="mb-4">{tax.description}</p>
-                    <p className="text-2xl font-bold">{tax.amount} €</p>
+                  <img src={`/images/taxes/${tax.title.replace(/\s+/g, '_').toLowerCase()}.jpg`} alt={tax.title} className="w-full h-48 sm:h-64 md:h-80 lg:h-[30rem] object-cover mb-4 rounded-xl" />
+                  <p className="text-2xl font-bold mb-4">{tax.title}</p>
+                  <p className="mb-4">{tax.description}</p>
+                  <p className="text-2xl font-bold">{tax.amount} €</p>
                 </div>
-            ))}
+              ))}
             </Slider>
-        </div>
-  </div>
-);
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
