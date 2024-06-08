c5bc87fb5cc5f84cac1fb11ce6d6f6260c1a6c8b
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-06-08 12:28:21 +0200

    Add - Navbar component to PaymentPage

--- frontend/src/app/payment/page.jsx
+++ frontend/src/app/payment/page.jsx
@@ -4,6 +4,7 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import Confetti from 'react-dom-confetti';
 import { useState } from 'react';
+import Navbar from '../../components/Navbar';
 import AlienAnimation from '../../components/AlienAnimation';
 import AnimatedTitle from '../../components/AnimatedTitle';
 
@@ -48,11 +49,80 @@ export default function PaymentPage() {
   });
 
   return (
-    <div className="container mx-auto mt-8">
+    <div className="container mx-auto">
+      <Navbar />
       <AnimatedTitle />
       <AlienAnimation />
       <form onSubmit={formik.handleSubmit}>
-        {/* Faut que je rajoute les champs du formulaire de paiement !!! */}
+        
+        <div className="mb-4 text-left relative">
+          <label className="block text-gray-700 font-bold mb-2">Numéro de règlement :</label>
+          <input
+            type="text"
+            name="paymentId"
+            value={formik.values.paymentId}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={`w-full px-4 py-2 rounded-lg ${
+              formik.touched.paymentId && formik.errors.paymentId ? 'border-red-500' : 'border-gray-300'
+            }`}
+          />
+          {formik.touched.paymentId && formik.errors.paymentId && (
+            <p className="text-red-500 text-xs italic">{formik.errors.paymentId}</p>
+          )}
+        </div>
+
+        <div className="mb-4 text-left relative">
+          <label className="block text-gray-700 font-bold mb-2">Numéro de carte :</label>
+          <input
+            type="text"
+            name="cardNumber"
+            value={formik.values.cardNumber}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={`w-full px-4 py-2 rounded-lg ${
+              formik.touched.cardNumber && formik.errors.cardNumber ? 'border-red-500' : 'border-gray-300'
+            }`}
+          />
+          {formik.touched.cardNumber && formik.errors.cardNumber && (
+            <p className="text-red-500 text-xs italic">{formik.errors.cardNumber}</p>
+          )}
+        </div>
+
+        <div className="mb-4 text-left relative">
+          <label className="block text-gray-700 font-bold mb-2">Cryptogramme :</label>
+          <input
+            type="text"
+            name="cryptogram"
+            value={formik.values.cryptogram}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={`w-full px-4 py-2 rounded-lg ${
+              formik.touched.cryptogram && formik.errors.cryptogram ? 'border-red-500' : 'border-gray-300'
+            }`}
+          />
+          {formik.touched.cryptogram && formik.errors.cryptogram && (
+            <p className="text-red-500 text-xs italic">{formik.errors.cryptogram}</p>
+          )}
+        </div>
+
+        <div className="mb-4 text-left relative">
+          <label className="block text-gray-700 font-bold mb-2">Date d'expiration :</label>
+          <input
+            type="text"
+            name="expirationDate"
+            value={formik.values.expirationDate}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={`w-full px-4 py-2 rounded-lg ${
+              formik.touched.expirationDate && formik.errors.expirationDate ? 'border-red-500' : 'border-gray-300'
+            }`}
+          />
+          {formik.touched.expirationDate && formik.errors.expirationDate && (
+            <p className="text-red-500 text-xs italic">{formik.errors.expirationDate}</p>
+          )}
+        </div>
+
         <button type="submit" className="bg-gradient-to-r from-yellow-300 to-yellow-500 hover:from-yellow-500 hover:to-yellow-700 text-white text-xl font-bold py-5 px-10 rounded-3xl focus:outline-none focus:shadow-outline" onClick={() => setConfettiActive(true)}>Payer</button>
       </form>
       <Confetti active={confettiActive} width={window.innerWidth} height={window.innerHeight} numberOfPieces={800} recycle={false} tweenDuration={10000} gravity={0.05}/>
