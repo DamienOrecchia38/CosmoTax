64ede0fe4c512003deb4fff80b9f3a91c1c6f2f4
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-06-08 12:27:33 +0200

    Refactor - ProfileController to update user profile information

--- backend/src/Controller/ProfileController.php
+++ backend/src/Controller/ProfileController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
 
 class ProfileController extends AbstractController
@@ -17,10 +19,31 @@ class ProfileController extends AbstractController
 
         return $this->json([
             'email' => $user->getEmail(),
-            'firstName' => $user->getFirstName(),
-            'lastName' => $user->getLastName(),  
+            'firstName' => $user->getFirstname(),
+            'lastName' => $user->getLastname(),
             'address' => $user->getAdress(),
             'phone' => $user->getPhone(),
+        ]);
+    }
+
+    #[Route('/api/profile', name: 'app_profile_update', methods: ['PUT'])]
+    public function update(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $data = json_decode($request->getContent(), true);
+
+        $user->setEmail($data['email']);
+        $user->setFirstname($data['firstName']);
+        $user->setLastname($data['lastName']);
+        $user->setAdress($data['address']);
+        $user->setPhone($data['phone']);
+
+        $entityManager->flush();
+
+        return $this->json([
+            'message' => 'Profil mis à jour avec succès',
         ]);
     }
 }
