f8183d06e051e1a4a05bdc53b8c31cfb5f21a870
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-06-13 11:15:37 +0200

    Update - navbar styling and add sticky behavior

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -45,7 +45,7 @@ export default function Navbar({ onLoginClick, onSignUpClick, onHomeClick }) {
   
       if (response.ok) {
         const data = await response.json();
-        const unpaid = data.filter(tax => !tax.paid).length;
+        const unpaid = data['hydra:member'].filter(tax => !tax.paid).length;
         setUnpaidTaxes(unpaid);
       }
     } catch (error) {
@@ -54,7 +54,7 @@ export default function Navbar({ onLoginClick, onSignUpClick, onHomeClick }) {
   };
 
   return (
-    <nav className="flex flex-col md:flex-row items-center justify-between p-4 bg-white bg-opacity-40">
+    <nav className="sticky top-0 z-50 flex flex-col items-center justify-between p-4 bg-white md:flex-row bg-opacity-40">
       <div className="flex items-center justify-between w-full md:w-auto">
         <Link href="/" className="flex items-center ml-4" onClick={onHomeClick}>
           <img src="/alien_navbar.png" alt="Alien logo" className="w-10 h-10 mr-2 sm:w-18 sm:h-18" />
@@ -67,17 +67,17 @@ export default function Navbar({ onLoginClick, onSignUpClick, onHomeClick }) {
       </div>
 
       <div className={`${isMenuOpen ? 'block' : 'hidden'} md:flex flex-col md:flex-row items-center justify-end w-full md:w-auto mt-4 md:mt-0 space-y-4 md:space-y-0 md:space-x-8`}>
-        <div className="flex flex-col md:flex-row items-center justify-end w-full md:w-auto space-y-4 md:space-y-0 md:space-x-8">
+        <div className="flex flex-col items-center justify-end w-full space-y-4 md:flex-row md:w-auto md:space-y-0 md:space-x-8">
           <script src="https://cdn.lordicon.com/lordicon.js"></script>
 
           {!isLoggedIn && (
             <>
-              <button className="flex items-center justify-center w-full md:w-auto px-5 py-1 text-xl font-bold text-gray-100 bg-gradient-to-r from-yellow-300 to-yellow-500 rounded-2xl hover:bg-yellow-500 ripple" onClick={onSignUpClick}>
+              <button className="flex items-center justify-center w-full px-5 py-1 text-xl font-bold text-gray-100 md:w-auto bg-gradient-to-r from-yellow-300 to-yellow-500 rounded-2xl hover:bg-yellow-500 ripple" onClick={onSignUpClick}>
                 <lord-icon src="https://cdn.lordicon.com/igljtrxq.json" trigger="loop-on-hover" stroke="bold" colors="primary:#c7c116,secondary:#c76f16" style={{ width: '30px', height: '30px', marginRight: '8px' }}></lord-icon>
                 Inscription
               </button>
 
-              <button className="flex items-center justify-center w-full md:w-auto px-5 py-1 text-xl font-bold text-gray-100 bg-gradient-to-r from-yellow-300 to-yellow-500 rounded-2xl hover:bg-yellow-500 ripple" onClick={onLoginClick}>
+              <button className="flex items-center justify-center w-full px-5 py-1 text-xl font-bold text-gray-100 md:w-auto bg-gradient-to-r from-yellow-300 to-yellow-500 rounded-2xl hover:bg-yellow-500 ripple" onClick={onLoginClick}>
                 <lord-icon src="https://cdn.lordicon.com/fygyhyze.json" trigger="loop-on-hover" stroke="bold" colors="primary:#c7c116,secondary:#c76f16" style={{ width: '30px', height: '30px', marginRight: '8px' }}></lord-icon>
                 Connexion
               </button>
@@ -87,7 +87,7 @@ export default function Navbar({ onLoginClick, onSignUpClick, onHomeClick }) {
           {isLoggedIn && (
             <>
               <Link href="/profile">
-                <button className="relative flex items-center justify-center w-full md:w-auto px-5 py-1 text-xl font-bold text-gray-100 bg-gradient-to-r from-yellow-300 to-yellow-500 rounded-2xl hover:bg-yellow-500 ripple">
+                <button className="relative flex items-center justify-center w-full px-5 py-1 text-xl font-bold text-gray-100 md:w-auto bg-gradient-to-r from-yellow-300 to-yellow-500 rounded-2xl hover:bg-yellow-500 ripple">
                   <lord-icon src="https://cdn.lordicon.com/xfzuyvam.json" trigger="loop-on-hover" stroke="bold" colors="primary:#c7c116,secondary:#c76f16" style={{ width: '30px', height: '30px', marginRight: '8px' }}></lord-icon>
                   {firstName ? firstName : 'Profil'}
                   {unpaidTaxes > 0 && (
@@ -99,14 +99,14 @@ export default function Navbar({ onLoginClick, onSignUpClick, onHomeClick }) {
               </Link>
 
               <Link href="/payment">
-                <button className="flex items-center justify-center w-full md:w-auto px-5 py-1 text-xl font-bold text-gray-100 bg-gradient-to-r from-yellow-300 to-yellow-500 rounded-2xl hover:bg-yellow-500 ripple">
+                <button className="flex items-center justify-center w-full px-5 py-1 text-xl font-bold text-gray-100 md:w-auto bg-gradient-to-r from-yellow-300 to-yellow-500 rounded-2xl hover:bg-yellow-500 ripple">
                   <lord-icon src="https://cdn.lordicon.com/fhszghjk.json" trigger="loop-on-hover" stroke="bold" colors="primary:#c7c116,secondary:#c76f16" style={{ width: '30px', height: '30px', marginRight: '8px' }}></lord-icon>
                   Paiement
                 </button>
               </Link>
 
               <Link href="/">
-                <button className="flex items-center justify-center w-full md:w-auto px-5 py-1 text-xl font-bold text-gray-100 bg-gradient-to-r from-yellow-300 to-yellow-500 rounded-2xl hover:bg-yellow-500 ripple" onClick={() => {localStorage.removeItem("token"); setIsLoggedIn(false);}}>
+                <button className="flex items-center justify-center w-full px-5 py-1 text-xl font-bold text-gray-100 md:w-auto bg-gradient-to-r from-yellow-300 to-yellow-500 rounded-2xl hover:bg-yellow-500 ripple" onClick={() => {localStorage.removeItem("token"); setIsLoggedIn(false);}}>
                   <lord-icon src="https://cdn.lordicon.com/peeuicbd.json" trigger="loop-on-hover" stroke="bold" colors="primary:#c7c116,secondary:#c76f16" style={{ width: '30px', height: '30px', marginRight: '8px' }}></lord-icon>
                   Déconnexion
                 </button>
